0fcfffb981e55cd38b88ec13b50d5398456670ea
justproto <just1proto@gmail.com>
2020-02-16 18:55:40 +0200

    added product and product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
 
 
+    var mixer = mixitup('.products__inner-box'); 
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false, 
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
-    
 }); //такая запись нужна, чтобы вначале выполнился код в index.html а потом уже в js 
