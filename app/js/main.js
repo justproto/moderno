83e46beefde9023346241739970d2c69b8ec503a
justproto <just1proto@gmail.com>
2020-02-21 01:44:50 +0200

    fix new items page, add icomoon

--- app/js/main.js
+++ app/js/main.js
@@ -25,13 +25,13 @@ $(function(){
     });
 
     $('.icon-th-list').on('click', function(){
-        $('.product__item').addClass('list');
+        $('.product-page__items .product__item').addClass('list');
         $('.icon-th-list').addClass('active');   
         $('.icon-th-large').removeClass('active');
     });
     
     $('.icon-th-large').on('click', function(){
-        $('.product__item').removeClass('list');
+        $('.product-page__items .product__item').removeClass('list');
         $('.icon-th-large').addClass('active');   
         $('.icon-th-list').removeClass('active');
     });
